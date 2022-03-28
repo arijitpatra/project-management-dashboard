2d0d7e878b154017b5bd8cc411137e2a86bf64ba
Arijit Patra <ari.clicks@gmail.com>
2022-03-29 02:02:37 +0530

    drag and drop
    drag and drop

--- src/components/BoardComponent/BoardComponent.js
+++ src/components/BoardComponent/BoardComponent.js
@@ -24,29 +24,50 @@ const BoardComponent = ({
     }
   };
 
+  // while drag and drop make sure you are updating the data
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const onDrop = (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("text");
+    console.log(data + title);
+    e.currentTarget.appendChild(document.getElementById(data));
+    e.dataTransfer.clearData("text");
+  };
+
+  const onDragStart = (e) => {
+    e.dataTransfer.setData("text/plain", e.target.id);
+  };
+
   return (
     <section className="board-component">
-      {title.length > 0 && (
-        <div className="d-f j-c-sb m-b-2d5">
-          <h3 className="m-05-0">{title}</h3>
-          <h3 className="m-05-0 cursor-pointer" onClick={boardDelete}>
-            X
-          </h3>
-        </div>
-      )}
+      <div onDrop={onDrop} onDragOver={onDragOver} id={title}>
+        {title.length > 0 && (
+          <div className="d-f j-c-sb m-b-2d5">
+            <h3 className="m-05-0">{title}</h3>
+            <h3 className="m-05-0 cursor-pointer" onClick={boardDelete}>
+              X
+            </h3>
+          </div>
+        )}
 
-      {cards.length > 0 && title.length > 0
-        ? cards.map((card, index) => {
-            return (
-              <CardComponent
-                title={card.title}
-                text={card.text}
-                key={card.title + index}
-                onCardDelete={() => cardDelete(card.id)}
-              />
-            );
-          })
-        : ""}
+        {cards.length > 0 && title.length > 0
+          ? cards.map((card, index) => {
+              return (
+                <CardComponent
+                  title={card.title}
+                  text={card.text}
+                  key={card.title + index}
+                  onCardDelete={() => cardDelete(card.id)}
+                  draggable={true}
+                  onDragStart={onDragStart}
+                />
+              );
+            })
+          : ""}
+      </div>
 
       {title.length > 0 ? (
         <div className="d-f f-d-c">
